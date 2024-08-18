f714a2be2a5f98c373d201221d1891a56e7b0fde
mdajazansari <mdajazansari2912@gmail.com>
2024-08-18 20:11:19 +0530

    Loops Questions
    Basic Loops Questions

---
+++ Driving License Test Ternary Operator/style.js
@@ -0,0 +1,7 @@
+// Print "You can drive" or "You can't drive" based on age being greater than 18 using ternary operator
+
+let age = prompt("Enter your age:")
+
+let license = (age > 18 ) ? "You can drive" : "You can't drive";
+
+console.log(license);
---
+++ Loops Questions/style.js
@@ -0,0 +1,38 @@
+/* Write a program to print the marks of a student in an object using for loop
+obj = { harry: 98, rohan: 70, aakash: 7}
+
+Write the program in Q1 using for in loop
+
+*/
+
+// let obj = {
+//     ajaz: 95,
+//     shaheen: 99,
+//     reyaz: 89,
+//     nobita: 10
+// }
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(element);
+// }
+
+
+// Write a program to print "try again" until user enters the correct number.
+
+
+// while (true){
+
+//     let num = parseInt(prompt("Enter a number:"));
+
+//     if(isNaN(num)){
+//         console.log("Please enter a valid number.")
+//     }
+//     else{
+//         console.log("Your number", num);
+//         break;
+//     }
+// }
+
+
+// Write a function to find mean of 5 numbers.
